025ef65398280db7a5c45bfa43fe17a25809246a
afarizap <afarizap@gmail.com>
2021-01-20 17:22:24 -0500

    Task 0  fix shebang and comments

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+""" import flask to print hello world from localhost """
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    print("holi")
+    """ return a string with hello world """
     return 'Hello, World!'
 
 
